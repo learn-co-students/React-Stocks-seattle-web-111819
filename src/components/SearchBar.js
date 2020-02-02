4a582cd93cabca5833617455a100299c2fba6fed
seanpgallivan <seanpgallivan@hotmail.com>
2020-02-01 16:25:13 -0800

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>
+const SearchBar = props => {
 
-      <strong>Sort by:</strong>
+  const handleSort = e => {
+    props.onSortChange(e.target.value)
+  }
+
+  const handleFilter = e => {
+    props.onFilterChange(e.target.value)
+  }
+
+  return (
+    <div style={{margin: "auto", width: "max-content"}}>
+
+      <strong>Sort by: </strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
+        <input type="radio" value="abc" checked={props.sort === "abc" ? true : false} onChange={handleSort}/>
+        Alphabetically 
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="$" checked={props.sort === "$" ? true : false} onChange={handleSort}/>
         Price
       </label>
       <br/>
 
       <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
+        <strong>Filter: </strong>
+        <select value={props.value} onChange={handleFilter}>
+          <option value="None">None</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,17 +3,16 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  displayStocks = () => this.props.stocks.map(stock => <Stock key={stock.id} stock={stock} onClickStock={this.props.onClickStock} />)
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {this.displayStocks()}
       </div>
     );
   }
-
 }
 
 export default PortfolioContainer;
