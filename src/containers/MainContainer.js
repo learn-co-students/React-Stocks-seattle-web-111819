9bedd10a5b8451b9cef723e7ee6b097957b5c54d
Miapan <miapanyuewei@gmail.com>
2020-02-02 18:36:19 -0800

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,76 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  //put state that tracks search bar changes
+  // state should have some string that represents a selection ie. "alphabetically", "by price"
+
+  state = {
+    stocks: [],
+    displayedStocks: [],
+    ownedStocks: [],
+    filteredStocks: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/stocks")
+    .then(res => res.json())
+    .then(data => this.setState({stocks: data, displayedStocks: data}))
+  }
+  
+  buyStock = selectedStock => {
+    const newOwnedStocks = [...this.state.ownedStocks]
+    if(this.state.ownedStocks.includes(selectedStock)){
+      let newer = newOwnedStocks.filter(stock => {
+        return stock.id !== selectedStock.id
+      })
+      this.setState({
+        ownedStocks: newer
+      })
+    }else{
+      newOwnedStocks.push(selectedStock)
+      this.setState({
+        ownedStocks: newOwnedStocks
+      })
+    }
+  }
+
+  filteredStocks = value => {
+    let filtered = this.state.stocks.filter(stock => stock.type === value)
+    this.setState({
+      displayedStocks: filtered
+    })
+  }
+
+  sortByName = stocks => {
+    let sorted = stocks.sort((a, b) => a.name.localeCompare(b.name))
+    this.setState({
+      displayedStocks: sorted
+    })
+  }
+
+  sortByPrice = stocks => {
+    let sorted = stocks.sort((a,b) => a.price > b.price ? 1 : -1)
+    this.setState({
+      displayedStocks: sorted
+    })
+    console.log(sorted)
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar onFilterStocks={this.filteredStocks} stocks={this.state.displayedStocks} onSortByName={this.sortByName} onSortByPrice={this.sortByPrice}/>
 
           <div className="row">
             <div className="col-8">
+              {/* in this component call you need to pass down the card array as well as the type selection ie. this.state.type */}
 
-              <StockContainer/>
+              <StockContainer allStocks={this.state.displayedStocks} onBuyStock={this.buyStock}  />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer ownedStocks={this.state.ownedStocks} onBuyStock={this.buyStock}/>
 
             </div>
           </div>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,26 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+
+
+
+  // write some logic that checks the type ie. this.props.type then sorts the card array dependent on the type that is being passed down
+
+  // write another function that takes the sorted cards array and maps over them to transform each element into a <Stock> component
+
+  mapStocks = () => {
+    return this.props.allStocks.map(stock => {
+      return <div><Stock key={stock.id} onBuyStock={this.props.onBuyStock} stock={stock} /></div>
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
           //render the list of stocks here
+          this.mapStocks()
         }
       </div>
     );
